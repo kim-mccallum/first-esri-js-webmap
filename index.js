eb82aee6e99842a2b1f25e50b185aa6bf27a3af4
Kim McCallum <kchaase@gmail.com>
2019-12-02 14:03:26 -0700

    added basemap gallery and feature layers

--- index.js
+++ index.js
@@ -1,23 +1,62 @@
 require([
     "esri/Map",
-    "esri/views/SceneView"
-    ], function(Map, SceneView) {
+    "esri/views/MapView",
+    "esri/widgets/BasemapToggle",
+    "esri/widgets/BasemapGallery", 
+    "esri/layers/FeatureLayer"
+  ], function(Map, MapView, BasemapToggle, BasemapGallery, FeatureLayer) {
 
     var map = new Map({
     basemap: "topo-vector",
     ground: "world-elevation"  // show elevation
     });
 
-    var view = new SceneView({
-    container: "viewDiv",
-    map: map,
-    camera: {
-        position: {  // observation point
-        x: -118.80800,
-        y: 33.96100,
-        z: 25000 // altitude in meters
-        },
-        tilt: 65  // perspective in degrees
-    }
+    // 2-d
+    var view = new MapView({
+        container: "viewDiv",
+        map: map,
+        center: [-118.80543,34.02700],
+        zoom: 13
+      });
+
+    // Add layers
+    // Trailheads feature layer (points)
+    var trailheadsLayer = new FeatureLayer({
+    url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads/FeatureServer/0"
     });
+
+    map.add(trailheadsLayer);
+    
+    // Trails feature layer (lines)
+    var trailsLayer = new FeatureLayer({
+    url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trails/FeatureServer/0"
+    });
+
+    map.add(trailsLayer, 0);
+
+    // Parks and open spaces (polygons)
+    var parksLayer = new FeatureLayer({
+    url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Parks_and_Open_Space/FeatureServer/0"
+    });
+
+    map.add(parksLayer, 0);
+
+    // Static
+    // var basemapToggle = new BasemapToggle({
+    //     view: view,
+    //     nextBasemap: "satellite"
+    //   });
+
+    // Toggle
+    var basemapGallery = new BasemapGallery({
+        view: view,
+        source: {
+          portal: {
+            url: "http://www.arcgis.com",
+            useVectorBasemaps: true, // Load vector tile basemap group
+          },
+        } 
+      });
+
+    view.ui.add(basemapGallery, "top-right"); // Add to the view
 });
